bab4994fc9816f54e2be0f8d7bedf79ade1c758e
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2024-01-29 16:56:35 +0300

    Внес изменения от Собира - все работает не так, как хотелось бы

--- materials/views.py
+++ materials/views.py
@@ -7,12 +7,15 @@ from materials.models import Material
 class MaterialCreateView(CreateView):
     model = Material
     fields = ('title', 'body',)
-    success_url = reverse_lazy('materials:list')
+    success_url = reverse_lazy('material:list')
+    template_name = 'materials/material_form.html'
 
 
 class MaterialListView(ListView):
     model = Material
+    template_name = 'materials/material_list.html'
 
 
 class MaterialDetailView(DetailView):
     model = Material
+    template_name = 'materials/material_detail.html'
